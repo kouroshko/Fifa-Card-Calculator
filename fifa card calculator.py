663c51e88b63520ce637f5ba072da44b3832608c
kouroshko <91155769+kouroshko@users.noreply.github.com>
2022-12-24 12:54:03 +0300

    First Commit

---
+++ fifa card calculator.py
@@ -0,0 +1,22 @@
+pac = input('Pac')
+
+sho = input('sho')
+
+pas = input('pas')
+
+dri = input('dri')
+
+defe = input('def')
+
+phy = input('phy')
+
+a = int(pac)
+b = int(sho)
+c = int(pas)
+d = int(dri)
+e = int(defe)
+f = int(phy)
+
+plus = (a + b + c + d + e + f)
+
+total = print(plus / 5)
